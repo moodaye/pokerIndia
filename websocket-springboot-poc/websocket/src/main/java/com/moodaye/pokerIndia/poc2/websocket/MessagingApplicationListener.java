01568231aa31e91fc4167c75b67df5abff9118e6
Mood Aye <rajivkewal@aol.com>
2020-12-15 21:58:59 -0500

    Now show random hole cards - more interesting basically

--- websocket-springboot-poc/websocket/src/main/java/com/moodaye/pokerIndia/poc2/websocket/MessagingApplicationListener.java
+++ websocket-springboot-poc/websocket/src/main/java/com/moodaye/pokerIndia/poc2/websocket/MessagingApplicationListener.java
@@ -1,5 +1,7 @@
 package com.moodaye.pokerIndia.poc2.websocket;
 
+import java.util.Random;
+
 import javax.annotation.PreDestroy;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class MessagingApplicationListener implements ApplicationListener<ContextRefreshedEvent> {
     private final MessageSendingOperations<String> messagingTemplate;
+    
+    private static String[] deck = {
+    		"As", "Ks", "Qs", "Js", "Ts", "9s", "8s", "7s", "6s", "5s", "4s", "3s", "2s",
+    		"Ah", "Kh", "Qh", "Jh", "Th", "9h", "8h", "7h", "6h", "5h", "4h", "3h", "2h",
+    		"Ad", "Kd", "Qd", "Jd", "Td", "9d", "8d", "7d", "6d", "5d", "4d", "3d", "2d",
+    		"Ac", "Kc", "Qc", "Jc", "Tc", "9c", "8c", "7c", "6c", "5c", "4c", "3c", "2c"};
+    		
 
     @Autowired
     public MessagingApplicationListener(MessageSendingOperations<String> messagingTemplate) {
@@ -22,7 +31,13 @@ public class MessagingApplicationListener implements ApplicationListener<Context
     }
 
     public void p() {
-        messagingTemplate.convertAndSend("/topic/greetings", new Greeting("Balloo!"));
+    	Random rand = new Random();
+    	int card1 = rand.nextInt() % 52;
+    	if(card1 < 0) card1 *= -1;
+    	int card2 = rand.nextInt() % 52;
+    	if(card2 < 0) card2 *= -1;
+    	
+        messagingTemplate.convertAndSend("/topic/greetings", new Greeting(deck[card1] + " " + deck[card2]));
     }
 
 }
