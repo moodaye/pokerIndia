a2de40809d067e7f4458d35b5db2aa3acfbc7ba6
Mood Aye <rajivkewal@aol.com>
2021-02-09 21:32:24 -0500

    MVP1-S1 This makes call - code review pending and final changes

--- pokerIndiaUI/src/app/app.component.ts
+++ pokerIndiaUI/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CreateGameService } from './services/create-game.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pokerIndiaUI';
+
+  constructor(private createGameService: CreateGameService){}
+
+  createGame(){
+    console.log("create game called");
+    this.createGameService.createGame().subscribe((resp) => console.log(resp));
+  }
 }
